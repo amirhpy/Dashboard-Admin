387ed1e6e5cc4a043502f3638300bdf8338a0ab6
amir hpr <amirhosseinhphpam@gmail.com>
2023-03-19 15:49:12 +0330

    Build Customers

--- src/Database/database.js
+++ src/Database/database.js
@@ -3,6 +3,11 @@ import marcus from '../Assets/Images/avatar/avatar-marcus-finn.png'
 import carson from '../Assets/Images/avatar/avatar-carson-darrin.png'
 import fran from '../Assets/Images/avatar/avatar-fran-perez.png'
 import jie from '../Assets/Images/avatar/avatar-jie-yan-song.png'
+import penjani from '../Assets/Images/avatar/avatar-penjani-inyene.png'
+import omar from '../Assets/Images/avatar/avatar-omar-darboe.png'
+import siegbert from '../Assets/Images/avatar/avatar-siegbert-gottfried.png'
+import iulia from '../Assets/Images/avatar/avatar-iulia-albu.png'
+import nasimiyu from '../Assets/Images/avatar/avatar-nasimiyu-danai.png'
 
 import healthcare from '../Assets/Images/products/product-1.png'
 import makeup from '../Assets/Images/products/product-2.png'
@@ -18,10 +23,22 @@ let inboxData = [
 ]
 
 let sellingProducts = [
-    {id: 1, img: healthcare, name: 'Healthcare Erbology', category: 'healthcare'},
-    {id: 2, img: makeup, name: 'Makeup Lancome Rouge', category: 'makeup'},
-    {id: 3, img: Necessaire, name: 'Skincare Necessaire', category: 'skincare'},
-    {id: 4, img: soja, name: 'Skincare Soja CO', category: 'skincare'}
+    { id: 1, img: healthcare, name: 'Healthcare Erbology', category: 'healthcare' },
+    { id: 2, img: makeup, name: 'Makeup Lancome Rouge', category: 'makeup' },
+    { id: 3, img: Necessaire, name: 'Skincare Necessaire', category: 'skincare' },
+    { id: 4, img: soja, name: 'Skincare Soja CO', category: 'skincare' }
 ]
 
-export { inboxData, sellingProducts }
+let users = [
+    { id: 1, profile: carson, name: 'Carson Darrin', email: 'Carson@gmail.com', location: 'Cleveland, Ohio, USA', orders: '3', spent: '$300,00' },
+    { id: 2, profile: fran, name: 'Fran Perez', email: 'Fran@gmail.com', location: 'Atlanta, Georgia, USA', orders: '4', spent: '$420,00' },
+    { id: 3, profile: jie, name: 'Jie Yan Song', email: 'Jie@gmail.com', location: 'North Canton, Ohio, USA', orders: '1', spent: '$110,00' },
+    { id: 4, profile: marcus, name: 'Marcus Finn', email: 'Marcus@gmail.com', location: 'Madrid, Madrid, Spain', orders: '2', spent: '$287,00' },
+    { id: 5, profile: penjani, name: 'Penjani Inyene', email: 'Penjani@gmail.com', location: 'Berkeley, California, USA', orders: '0', spent: '$0,00' },
+    { id: 6, profile: omar, name: 'Omar Darobe', email: 'Omar@gmail.com', location: 'Carson City, Nevada, USA', orders: '10', spent: '$110,00' },
+    { id: 7, profile: siegbert, name: 'Siegbert Gottfried', email: 'Siegbert@gmail.com', location: 'Los Angeles, California, USA', orders: '2', spent: '$1,000,000' },
+    { id: 8, profile: iulia, name: 'Iulia Albu', email: 'Iulia@gmail.com', location: 'Murray, Utah, USA', orders: '0', spent: '$0,00' },
+    { id: 9, profile: nasimiyu, name: 'Nasimiyu Danai', email: 'Nasimiyu@gmail.com', location: 'Salt Lake City, Utah, USA', orders: '7', spent: '$200,00' },
+]
+
+export { inboxData, sellingProducts, users }
--- src/Pages/Customers/Customers.js
+++ src/Pages/Customers/Customers.js
@@ -1,9 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+// database
+import { users } from '../../Database/database';
+
+// MUI DataGrid
+import { DataGrid } from '@mui/x-data-grid'
+import Box from '@mui/material/Box'
+import Avatar from '@mui/material/Avatar'
+
+// MUI Icon
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
 
 const Customers = () => {
+    const [usersData] = useState(users)
+
+    const columns = [
+        {
+            field: 'field',
+            headerName: 'Name',
+            width: 300,
+            renderCell: (params) => {
+                return (
+                    <Link to='/'>
+                        <div className='customers-users'>
+                            <Avatar src={params.row.profile} alt='profile' />
+                            <div className='customers-username-content'>
+                                <p className='customers__name'>{params.row.name}</p>
+                                <p className='customers__email'>{params.row.email}</p>
+                            </div>
+                        </div>
+                    </Link>
+                )
+            }
+        },
+        {
+            field: 'location',
+            headerName: 'Location',
+            cellClassName: 'customers__cell-style',
+            width: 250
+        },
+        {
+            field: 'orders',
+            headerName: 'Orders',
+            cellClassName: 'customers__cell-style',
+            width: 150
+        },
+        {
+            field: 'spent',
+            headerName: 'Spent',
+            cellClassName: 'customers__cell-style',
+            width: 150
+        },
+        {
+            field: 'action',
+            headerName: 'Actions',
+            cellClassName: 'customers__cell-style',
+            width: 80,
+            renderCell: (params) => {
+                return (
+                    <div className='customers__actions'>
+                        <Link to='/'>
+                            <ModeEditOutlineOutlinedIcon />
+                        </Link>
+                        <Link to='/'>
+                            <ArrowForwardIcon />
+                        </Link>
+                    </div>
+                )
+            }
+        }
+    ]
+
     return (
-        <div>
-            Customers
+        <div className='customers'>
+            <div className='container'>
+                <h3 className='components-name'>Customers</h3>
+                <Box sx={{ height: '40rem', width: '100%' }}>
+                    <DataGrid
+                        sx={{
+                            color: '#2f3746',
+                            fontSize: '1.2rem',
+                            fontFamily: 'inter-semibold'
+                        }}
+                        className='shadow-c'
+                        autoHeight
+                        rows={usersData}
+                        columns={columns}
+                        initialState={{
+                            pagination: {
+                                paginationModel: {
+                                    pageSize: 5,
+                                },
+                            },
+                        }}
+                        pageSizeOptions={[5]}
+                        checkboxSelection
+                        disableRowSelectionOnClick
+                    />
+                </Box>
+            </div>
         </div>
     );
 };
